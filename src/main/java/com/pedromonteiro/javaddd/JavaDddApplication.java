54247b64deacc3c5dc520e0e8e3adc3d0d03c007
Pedro Monteiro <pedromont436@gmail.com>
2024-02-09 10:57:13 -0300

    primeiro commit

---
+++ src/main/java/com/pedromonteiro/javaddd/JavaDddApplication.java
@@ -0,0 +1,13 @@
+package com.pedromonteiro.javaddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedromonteiro/javaddd/JavaDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromonteiro.javaddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
